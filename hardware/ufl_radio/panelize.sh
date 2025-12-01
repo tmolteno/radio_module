a0ba4618add8930b5173716cf7b0b871a09097da
Tim Molteno <tim@elec.ac.nz>
2025-12-01 14:24:44 +1300

    Add a better panelize script and get the impedance right

--- hardware/ufl_radio/panelize.sh
+++ hardware/ufl_radio/panelize.sh
@@ -3,7 +3,11 @@ kikit panelize \
     --tabs 'type: fixed; vwidth: 10mm; hwidth: 10mm; vcount: 2; hcount: 2' \
     --cuts 'type: vcuts' \
     --framing 'type: railstb; mintotalheight: 100mm; width: 5mm' \
-    --fiducials 'type: 4fid; hoffset: 5mm; voffset: 3.35mm; coppersize: 1mm; opening: 2mm' \
+    --fiducials 'type: 4fid; hoffset: 10mm; voffset: 3.35mm; coppersize: 1mm; opening: 2mm' \
+    --tooling 'type: 3hole; hoffset: 5mm; voffset: 2.5mm; size: 1.152mm; solderMaskMargin: 1.3mm' \
     --text 'type: simple; voffset: 2.5mm; text: TART Radio Module Set' \
     --post 'millradius: 0.5mm' \
     ufl_radio.kicad_pcb panelized_radios.kicad_pcb
+
+
+
