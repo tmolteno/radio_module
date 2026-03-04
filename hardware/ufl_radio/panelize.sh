6d66e47780a7e59a62b175b63d309324edce999a
Tim Molteno <tim@elec.ac.nz>
2026-03-05 10:09:14 +1300

    Add vcuts to the Edge.Cuts layer

--- hardware/ufl_radio/panelize.sh
+++ hardware/ufl_radio/panelize.sh
@@ -1,7 +1,7 @@
 kikit panelize \
     --layout 'alternation: cols; hspace: 2mm; vspace: 2mm; rotation: 180deg; rows: 3; cols: 2' \
     --tabs 'type: fixed; vwidth: 10mm; hwidth: 10mm; vcount: 2; hcount: 2' \
-    --cuts 'type: vcuts' \
+    --cuts 'type: vcuts; layer: Edge.Cuts' \
     --framing 'type: railstb; mintotalheight: 100mm; width: 5mm' \
     --fiducials 'type: 4fid; hoffset: 10mm; voffset: 3.35mm; coppersize: 1mm; opening: 2mm' \
     --tooling 'type: 3hole; hoffset: 5mm; voffset: 2.5mm; size: 1.152mm; solderMaskMargin: 1.3mm' \
